902970f299259d7cb5c750dc0f673ceec543a375
rushmi0 <ph.watcharapol_st@tni.ac.th>
2023-12-19 01:51:02 +0700

    จะนอนละ

--- src/main/kotlin/org/jungmha/Application.kt
+++ src/main/kotlin/org/jungmha/Application.kt
@@ -3,26 +3,25 @@ package org.jungmha
 import io.micronaut.runtime.Micronaut
 import io.swagger.v3.oas.annotations.*
 import io.swagger.v3.oas.annotations.info.*
+import org.slf4j.LoggerFactory
 
 
 @OpenAPIDefinition(
     info = Info(
-            title = "jungmha",
-            version = "0.1"
+        title = "jungmha",
+        version = "0.1"
     )
 )
 object Api {
+
 }
 
-
 object Application {
-
-//    private val LOG = LoggerFactory.getLogger(Application::class.java)
+    private val LOG = LoggerFactory.getLogger(Application::class.java)
 
     @JvmStatic
     fun main(args: Array<String>) {
-        println("c5234b2313bc9e7bdbf18810e12b41f636588c0eeef8ce4bc31de57c120a7d73".length)
+        LOG.info("Thread ${Thread.currentThread().name} executing main in Application")
         Micronaut.run(Application.javaClass)
     }
-
 }
