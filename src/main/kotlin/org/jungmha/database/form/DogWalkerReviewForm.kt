75d2f533986f30d0b88fc24d5c43d11f1a3d8243
rushmi0 <ph.watcharapol_st@tni.ac.th>
2023-12-20 19:25:40 +0700

    implemented สำเร็จพร้อมใช้งาน

--- src/main/kotlin/org/jungmha/database/form/DogWalkerReviewForm.kt
+++ src/main/kotlin/org/jungmha/database/form/DogWalkerReviewForm.kt
@@ -6,6 +6,8 @@ import io.micronaut.serde.annotation.Serdeable
 @Introspected
 @Serdeable.Deserializable
 data class DogWalkerReviewForm(
+    val walkerID: Int,
+    val userID: Int,
     val rating: Int,
     val reviewText: String
 )
--- src/main/kotlin/org/jungmha/database/statement/DogWalkerReviewServiceImpl.kt
+++ src/main/kotlin/org/jungmha/database/statement/DogWalkerReviewServiceImpl.kt
@@ -4,8 +4,13 @@ import io.micronaut.context.annotation.Bean
 import io.micronaut.runtime.http.scope.RequestScope
 import io.micronaut.scheduling.TaskExecutors
 import io.micronaut.scheduling.annotation.ExecuteOn
+import jakarta.inject.Inject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.jooq.DSLContext
 import org.jungmha.database.field.DogWalkerReviewField
 import org.jungmha.database.form.DogWalkerReviewForm
+import org.jungmha.infra.database.tables.Dogwalkerreviews.DOGWALKERREVIEWS
 import org.jungmha.service.DogWalkerReviewService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -13,26 +18,141 @@ import org.slf4j.LoggerFactory
 @Bean
 @RequestScope
 @ExecuteOn(TaskExecutors.IO)
-class DogWalkerReviewServiceImpl : DogWalkerReviewService {
+class DogWalkerReviewServiceImpl @Inject constructor(
+    private val query: DSLContext
+) : DogWalkerReviewService {
 
     override suspend fun dogWalkerReviewAll(): List<DogWalkerReviewField> {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO) {
+            val currentThreadName = Thread.currentThread().name
+
+            try {
+                LOG.info("Retrieve dog walker reviews operation started on thread [$currentThreadName]")
+
+                val data = query.select()
+                    .from(DOGWALKERREVIEWS)
+
+                val result = data.map { record ->
+                    DogWalkerReviewField(
+                        record[DOGWALKERREVIEWS.REVIEW_ID],
+                        record[DOGWALKERREVIEWS.WALKER_ID],
+                        record[DOGWALKERREVIEWS.USER_ID],
+                        record[DOGWALKERREVIEWS.RATING],
+                        record[DOGWALKERREVIEWS.REVIEW_TEXT]
+                    )
+                }
+
+                if (result.isNotEmpty()) {
+                    LOG.info("Retrieve dog walker reviews operation successful on thread [$currentThreadName]")
+                } else {
+                    LOG.warn("No dog walker reviews found on thread [$currentThreadName]")
+                }
+
+                return@withContext result
+            } catch (e: Exception) {
+                LOG.error("Error during retrieve dog walker reviews operation on thread [$currentThreadName]", e)
+                return@withContext emptyList()
+            }
+        }
     }
 
     override suspend fun insert(payload: DogWalkerReviewForm): Boolean {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO) {
+            val currentThreadName = Thread.currentThread().name
+
+            try {
+                LOG.info("Insert dog walker review operation started on thread [$currentThreadName]")
+
+                val result = query.insertInto(
+                    DOGWALKERREVIEWS,
+                    DOGWALKERREVIEWS.WALKER_ID,
+                    DOGWALKERREVIEWS.USER_ID,
+                    DOGWALKERREVIEWS.RATING,
+                    DOGWALKERREVIEWS.REVIEW_TEXT
+                )
+                    .values(
+                        payload.walkerID,
+                        payload.userID,
+                        payload.rating,
+                        payload.reviewText
+                    )
+                    .execute()
+
+                if (result > 0) {
+                    LOG.info("Insert dog walker review successful on thread [$currentThreadName]")
+                } else {
+                    LOG.warn("Insert dog walker review did not affect any rows on thread [$currentThreadName]")
+                }
+
+                return@withContext result > 0
+            } catch (e: Exception) {
+                LOG.error("Error during insert dog walker review operation on thread [$currentThreadName]", e)
+                return@withContext false
+            }
+        }
     }
 
     override suspend fun update(id: Int, fieldName: String, newValue: String): Boolean {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO) {
+            try {
+                val status = when (fieldName) {
+                    "walkerID" -> query.update(DOGWALKERREVIEWS)
+                        .set(DOGWALKERREVIEWS.WALKER_ID, Integer.valueOf(newValue))
+                    "userID" -> query.update(DOGWALKERREVIEWS)
+                        .set(DOGWALKERREVIEWS.USER_ID, Integer.valueOf(newValue))
+                    "rating" -> query.update(DOGWALKERREVIEWS)
+                        .set(DOGWALKERREVIEWS.RATING, Integer.valueOf(newValue))
+                    "reviewText" -> query.update(DOGWALKERREVIEWS)
+                        .set(DOGWALKERREVIEWS.REVIEW_TEXT, newValue)
+                    else -> {
+                        LOG.error("Field name [$fieldName] not found!!!")
+                        return@withContext false
+                    }
+                }
+
+                val affectedRows = status.where(DOGWALKERREVIEWS.REVIEW_ID.eq(id)).execute()
+
+                if (affectedRows > 0) {
+                    LOG.info("Update successful for field [$fieldName] with new value [$newValue] for Dog Walker Review ID [$id]")
+                } else {
+                    LOG.error("Update did not affect any rows for field [$fieldName] with new value [$newValue] for Dog Walker Review ID [$id]")
+                }
+
+                return@withContext affectedRows > 0
+            } catch (e: Exception) {
+                LOG.error("An error occurred during update", e)
+                return@withContext false
+            }
+        }
     }
 
+
     override suspend fun delete(id: Int): Boolean {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO) {
+            val currentThreadName = Thread.currentThread().name
+
+            try {
+                LOG.info("Delete dog walker review operation started on thread [$currentThreadName]")
+
+                val result = query.deleteFrom(DOGWALKERREVIEWS)
+                    .where(DOGWALKERREVIEWS.REVIEW_ID.eq(id))
+                    .execute()
+
+                if (result > 0) {
+                    LOG.info("Delete dog walker review successful for Dog Walker Review ID [$id] on thread [$currentThreadName]")
+                } else {
+                    LOG.warn("Delete dog walker review did not affect any rows for Dog Walker Review ID [$id] on thread [$currentThreadName]")
+                }
+
+                return@withContext result > 0
+            } catch (e: Exception) {
+                LOG.error("Error during delete dog walker review operation for Dog Walker Review ID [$id] on thread [$currentThreadName]", e)
+                return@withContext false
+            }
+        }
     }
 
     companion object {
         private val LOG: Logger = LoggerFactory.getLogger(DogWalkerReviewServiceImpl::class.java)
     }
-
 }
