989404e2f53db825d18f9abe704685e32289918a
rushmi0 <ph.watcharapol_st@tni.ac.th>
2023-12-22 13:08:01 +0700

    Shared Key

---
+++ src/main/kotlin/org/jungmha/security/securekey/ECDHkey.kt
@@ -0,0 +1,65 @@
+package org.jungmha.security.securekey
+
+import io.micronaut.context.annotation.Bean
+import io.micronaut.runtime.http.scope.RequestScope
+import io.micronaut.scheduling.TaskExecutors
+import io.micronaut.scheduling.annotation.ExecuteOn
+import org.jungmha.security.securekey.ECPublicKey.compressed
+import org.jungmha.security.securekey.ECPublicKey.pointRecovery
+import org.jungmha.security.securekey.ECPublicKey.toPublicKey
+import org.jungmha.security.securekey.EllipticCurve.multiplyPoint
+import java.math.BigInteger
+
+@Bean
+@RequestScope
+@ExecuteOn(TaskExecutors.IO)
+class ECDHkey {
+
+    /**
+     * `ECDH Key` มีชื่อเรียกเฉพาะคือ `ECDH Shared Secret` หรือ `Elliptic Curve Diffie-Hellman Shared Secret`
+     * `ECDH Shared Secret` คือคีย์ลับที่สร้างขึ้นระหว่างสองฝ่ายโดยใช้อัลกอริทึม ECDH (Elliptic Curve Diffie-Hellman)
+     * โดยใช้คีย์สาธารณะและคีย์ส่วนตัวของฝ่ายแต่ละฝ่าย นำมาคำนวณกัน จะได้คีย์ลับที่เป็นค่าเดียวกัน ซึ่งจะนำไปใช้เป็นคีย์สำหรับการเข้ารหัสและถอดรหัสข้อมูล โดยใช้วิธีการเข้ารหัสแบบสมมาตร (Symmetric Encryption)
+     * */
+
+
+
+    // ใช้สำหรับสร้าง Shared Key ระหว่าง 2 ฝ่าย เรียกว่า ECDH (Elliptic Curve Diffie-Hellman)
+    fun sharedSecret(
+        // Private Key ของตัวเอง
+        privateKey: BigInteger,
+        // Public Key ของฝ่ายตรงข้าม
+        publicKey: String
+    ): String {
+
+        // แปลง public key ให้อยู่ในรูปของ PointField นั้นก็คือ (x, y) ซึ่งเป็นพิกัดบนเส้นโค้งวงรี
+        val point: PointField = publicKey.pointRecovery()
+            ?: throw IllegalArgumentException("Invalid or unsupported public key format")
+
+        // คำนวณค่าจุดบนเส้นโค้งวงรีจาก private key โดยใช้เมธอด `generatePoint` ที่เขียนไว้ใน `ECPublicKey.kt`
+        val curvePoint = multiplyPoint(
+            privateKey,
+            point
+        )
+
+        // เอาเฉพาะพิกัด x และแปลงเป็นเลขฐาน 16 ก่อนคืนค่ากลับไป
+        return curvePoint.x.toString(16)
+    }
+
+}
+
+fun main() {
+
+    val privateKey = BigInteger("B885C70F190320D90AAECDBED18E4CB556D52AA9D8CD3E4040EC1582A960C43B", 16)
+    val publicKey = privateKey.toPublicKey().compressed()
+
+    println(publicKey)
+
+    val ecdh = ECDHkey()
+
+    val sharedKey = ecdh.sharedSecret(
+        privateKey,
+        "0321d0667dcc8f0cc83eda3c11a116ff315d995681b694f1d692fbde5f7c2fcf91"
+    )
+    println(sharedKey)
+
+}
