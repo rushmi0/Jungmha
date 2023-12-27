27e623300e15f617fc8ced6f8ae872f20e29af94
rushmi0 <ph.watcharapol_st@tni.ac.th>
2023-12-27 17:53:59 +0700

    `kaizen` ระบบ filter

--- src/main/kotlin/org/jungmha/routes/api/v1/home/PrivateFilterController.kt
+++ src/main/kotlin/org/jungmha/routes/api/v1/home/PrivateFilterController.kt
@@ -13,15 +13,13 @@ import io.micronaut.scheduling.annotation.ExecuteOn
 import jakarta.inject.Inject
 import org.jungmha.database.statement.DogsWalkersServiceImpl
 import org.jungmha.security.securekey.Token
+import org.jungmha.security.securekey.TokenObject
 import org.slf4j.LoggerFactory
 import java.util.*
 import java.util.stream.Collectors
 
 // * Private Filter Controller
 
-/**
- * คลาสนี้เป็น Controller สำหรับการกรองข้อมูล Private Dog Walker
- */
 @Controller("api/v1")
 @Bean
 @RequestScope
@@ -31,25 +29,12 @@ class PrivateFilterController @Inject constructor(
     private val token: Token
 ) {
 
-    /**
-     * เมธอดสำหรับการดึงข้อมูล Private Dog Walker ตามเงื่อนไขที่ระบุ
-     *
-     * @param access ข้อมูล Token ที่ใช้ในการตรวจสอบสิทธิ์
-     * @param name ชื่อของ Dog Walker (ถ้ามี)
-     * @param verify สถานะการตรวจสอบ (ถ้ามี)
-     * @param location สถานที่ที่ Dog Walker ทำงาน (ถ้ามี)
-     * @param pSmall ราคาต่ำสุดสำหรับขนาดเล็ก (ถ้ามี)
-     * @param pMedium ราคาต่ำสุดสำหรับขนาดกลาง (ถ้ามี)
-     * @param pBig ราคาต่ำสุดสำหรับขนาดใหญ่ (ถ้ามี)
-     * @param max จำนวนข้อมูลสูงสุดที่ต้องการแสดงผล (ถ้าไม่ระบุให้ใช้ค่า Integer.MAX_VALUE)
-     * @return ข้อมูลสำหรับผู้ที่ลงทะเบียนแล้ว ตามเงื่อนไขที่ระบุ
-     */
     @Get(
         uri = "/auth/home/filter{?verify,location,name,pSmall,pMedium,pBig,max}",
         produces = [MediaType.APPLICATION_JSON]
     )
     suspend fun getPrivateDogWalker(
-        //@Header("Authorization") access: String,
+        @Header("Authorization") access: String,
         @QueryValue("name") name: Optional<String>,
         @QueryValue("verify") verify: Optional<String>,
         @QueryValue("location") location: Optional<String>,
@@ -59,43 +44,38 @@ class PrivateFilterController @Inject constructor(
         @QueryValue("max") max: Optional<Int> = Optional.of(Integer.MAX_VALUE)
     ): Any? {
 
-        //val verifyToken = token.verifyToken(access)
-
         try {
+            // ตรวจสอบความถูกต้องของ Token
+            val userDetails: TokenObject = token.viewDetail(access)
+            val permission: String = userDetails.permission
 
-            return if (true) {
-                // ดึงข้อมูล DogWalker ทั้งหมดจากฐานข้อมูล
-                val rawData = service.privateDogWalkersAll()
-
-                // กรองข้อมูลตามเงื่อนไข
-                rawData.stream()
-                    .filter { data ->
-                        // ตรวจสอบค่า verify หากไม่ระบุหรือระบุถูกต้อง
-                        val verifyMatch = !verify.isPresent || data.detail.verify.equals(verify.get(), ignoreCase = true)
-
-                        // ตรวจสอบค่า name หากไม่ระบุหรือระบุถูกต้อง
-                        val nameMatch = !name.isPresent || data.detail.name.equals(name.get(), ignoreCase = true)
-
-                        // ตรวจสอบค่า location หากไม่ระบุหรือระบุถูกต้อง
-                        val locationMatch = !location.isPresent || data.detail.location.equals(location.get(), ignoreCase = true)
-
-                        // ตรวจสอบค่า price ตามช่วงราคาที่ระบุ
-                        val pSmallMatch = (!pSmall.isPresent || data.detail.price.small <= pSmall.get())
-                        val pMediumMatch = (!pMedium.isPresent || data.detail.price.medium <= pMedium.get())
-                        val pBigMatch = (!pBig.isPresent || data.detail.price.big <= pBig.get())
-
-                        // กรองข้อมูลตามเงื่อนไข
-                        verifyMatch && nameMatch && locationMatch && pSmallMatch && pMediumMatch && pBigMatch
-                    }
-                    .limit(max.orElse(Integer.MAX_VALUE).toLong()) // จำกัดจำนวนสูงสุดที่ต้องการแสดงผล
-                    .collect(Collectors.toList())
-            } else {
-                HttpResponse.badRequest("Invalid Token")
+            // ตรวจสอบสิทธิ์การใช้งาน
+            if (!token.verifyToken(access) || permission != "view-only") {
+                LOG.warn("Invalid token for getting Private Dog Walker")
+                return HttpResponse.badRequest("Invalid token")
             }
 
+            // ดึงข้อมูล DogWalker ทั้งหมดจากฐานข้อมูล
+            val rawData = service.privateDogWalkersAll()
+
+            // กรองข้อมูลตามเงื่อนไข
+            val filteredData = rawData.stream()
+                .filter { data ->
+                    val verifyMatch = !verify.isPresent || data.detail.verify.equals(verify.get(), ignoreCase = true)
+                    val nameMatch = !name.isPresent || data.detail.name.equals(name.get(), ignoreCase = true)
+                    val locationMatch = !location.isPresent || data.detail.location.equals(location.get(), ignoreCase = true)
+                    val pSmallMatch = !pSmall.isPresent || data.detail.price.small <= pSmall.get()
+                    val pMediumMatch = !pMedium.isPresent || data.detail.price.medium <= pMedium.get()
+                    val pBigMatch = !pBig.isPresent || data.detail.price.big <= pBig.get()
+
+                    verifyMatch && nameMatch && locationMatch && pSmallMatch && pMediumMatch && pBigMatch
+                }
+                .limit(max.orElse(Integer.MAX_VALUE).toLong())
+                .collect(Collectors.toList())
+
+            return HttpResponse.ok(filteredData)
         } catch (e: Exception) {
             LOG.error("Error during getPrivateDogWalker operation: ${e.message}", e)
-            // เพิ่มการแสดง path file ที่มีปัญหา
             LOG.error("Error file path: ${e.stackTrace.joinToString("\n")}")
             return HttpResponse.serverError("Internal Server Error: ${e.message}")
         }
@@ -103,7 +83,6 @@ class PrivateFilterController @Inject constructor(
 
 
     companion object {
-        // Logger สำหรับการดึงข้อมูล Private Dog Walker
         private val LOG = LoggerFactory.getLogger(PrivateFilterController::class.java)
     }
 
