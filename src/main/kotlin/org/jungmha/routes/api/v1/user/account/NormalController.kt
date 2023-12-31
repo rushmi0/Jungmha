2aac93c732147abf94253e64c89b4374d55cf6dc
rushmi0 <ph.watcharapol_st@tni.ac.th>
2023-12-31 17:40:06 +0700

    `kaizen` ระบบเรียกดูข้อมูลส่วนบุคคลของ NormalController

--- src/main/kotlin/org/jungmha/routes/api/v1/user/account/NormalController.kt
+++ src/main/kotlin/org/jungmha/routes/api/v1/user/account/NormalController.kt
@@ -12,89 +12,96 @@ import io.micronaut.runtime.http.scope.RequestScope
 import io.micronaut.scheduling.TaskExecutors
 import io.micronaut.scheduling.annotation.ExecuteOn
 import jakarta.inject.Inject
+import kotlinx.coroutines.coroutineScope
 import org.jungmha.database.statement.UserServiceImpl
 import org.jungmha.domain.response.EncryptedData
+import org.jungmha.domain.response.NormalInfo
 import org.jungmha.security.securekey.AES
 import org.jungmha.security.securekey.Token
 import org.jungmha.security.securekey.TokenObject
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-
 @Controller("api/v1")
 @Bean
 @RequestScope
 @ExecuteOn(TaskExecutors.IO)
 class NormalController @Inject constructor(
-    private val  userService: UserServiceImpl,
+    private val userService: UserServiceImpl,
     private val token: Token,
     private val aes: AES
 ) {
 
-
     @Get(
-        uri = "auth/user/normal/{nameDemo}",
+        "auth/user/normal",
         produces = [MediaType.APPLICATION_JSON]
     )
     suspend fun getPersonalInfo(
-        @Header("Access-Token") access: String,
-        nameDemo: String
-    ): MutableHttpResponse<Any>? {
-        // ตรวจสอบความถูกต้องของ Token
-        val userDetails: TokenObject = token.viewDetail(access)
-        val permission: String = userDetails.permission
-        val name = userDetails.userName
+        @Header("Access-Token") access: String
+    ): MutableHttpResponse<out Any?>? {
+        return try {
+            // ตรวจสอบความถูกต้องของโทเค็นและการอนุญาตของผู้ใช้
+            val userDetails: TokenObject = token.viewDetail(access)
+            val verify = token.verifyToken(access)
+            val permission: String = userDetails.permission
+            val name = userDetails.userName
 
-        try {
-            // ตรวจสอบสิทธิ์การใช้งาน
-            if (!token.verifyToken(access) || permission != "view") {
-                LOG.warn("Invalid token for user: $name")
-                return HttpResponse.badRequest("Invalid token")
+            // ตรวจสอบความถูกต้องของ Token และสิทธิ์การใช้งาน
+            return if (verify && permission == "view") {
+                coroutineScope {
+                    processSearching(name)
+                }
+            } else {
+                LOG.warn("Invalid token or insufficient permission for user: $name")
+                HttpResponse.badRequest("Invalid token or insufficient permission")
             }
-
-            // เรียกดูข้อมูล ขอบัญชีผู้ใช้คนนั้นๆ
-            val userInfo = userService.getUserInfo(nameDemo) ?: run {
-                LOG.warn("User info not found for user: $name")
-                return HttpResponse.notFound("User info not found")
-            }
-
-            val shareKey = userService.findUser(name)?.sharedKey.toString()
-
-            // นำข้อมูลมา Encrypt
-            val encrypted = aes.encrypt(userInfo.toString(), shareKey)
-            val data = EncryptedData(encrypted)
-
-            LOG.info("Personal info retrieved successfully for user: $name")
-            return HttpResponse.ok(userInfo)
         } catch (e: Exception) {
-            LOG.error("Error retrieving personal info for user: $name", e)
-            return HttpResponse.serverError("Failed to retrieve personal info")
+            LOG.error("Error processing getPersonalInfo", e)
+            HttpResponse.serverError("Internal server error: ${e.message}")
         }
     }
 
+    private suspend fun processSearching(name: String): MutableHttpResponse<out Any?> {
+        // เรียกดูข้อมูล ขอบัญชีผู้ใช้คนนั้นๆ
+        val userInfo: NormalInfo? = userService.getUserInfo(name)
 
+        return if (userInfo != null) {
+            userInfo.processEncrypting(name)
+        } else {
+            LOG.warn("User info not found for user: $name")
+            HttpResponse.notFound("User info not found")
+        }
+    }
+
+    private suspend fun NormalInfo.processEncrypting(name: String): MutableHttpResponse<out Any?> {
+        // นำข้อมูลมา Encrypt
+        val shareKey = userService.findUser(name)?.sharedKey.toString()
+        val encrypted = aes.encrypt(this.toString(), shareKey)
+        return HttpResponse.ok(EncryptedData(encrypted))
+        //return HttpResponse.ok(this)
+    }
+
+
+    // �� ──────────────────────────────────────────────────────────────────────────────────────── �� \\
 
 
     @Patch(
-        uri = "auth/user/normal",
+        "auth/user/normal",
         consumes = [MediaType.APPLICATION_JSON],
         produces = [MediaType.APPLICATION_JSON]
     )
-    suspend fun editPersonalInfo(
-        @Header("Access-Token") access: String
-    ) {
-
+    suspend fun editPersonalInfo(@Header("Access-Token") access: String) {
         // ตรวจสอบความถูกต้องของ Token
         val userDetails: TokenObject = token.viewDetail(access)
         val permission: String = userDetails.permission
 
         // ตรวจสอบสิทธิ์การใช้งาน
         if (!token.verifyToken(access) || permission != "edit") {
-            LOG.warn("Invalid token")
+            LOG.warn("Invalid token or insufficient permission during editPersonalInfo")
+        } else {
+            LOG.info("User ${userDetails.userName} is editing personal information")
         }
-
     }
-
 
     companion object {
         val LOG: Logger = LoggerFactory.getLogger(NormalController::class.java)
