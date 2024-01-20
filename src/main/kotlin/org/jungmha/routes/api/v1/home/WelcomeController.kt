b4bc077cce0a59a9e31386c74388079b4d1c52c3
rushmi0 <ph.watcharapol_st@tni.ac.th>
2024-01-20 18:03:34 +0700

    Share Static Resources

--- src/main/kotlin/org/jungmha/routes/api/v1/home/WelcomeController.kt
+++ src/main/kotlin/org/jungmha/routes/api/v1/home/WelcomeController.kt
@@ -7,13 +7,44 @@ import io.micronaut.http.annotation.Get
 import java.nio.charset.Charset
 
 @Bean
-@Controller
+@Controller("/")
 class WelcomeController {
 
+    private val resource = javaClass.getResourceAsStream("/public/index.html")
+        ?.readBytes()
+        ?.toString(Charset.defaultCharset())
+
     @Get(uri = "/", produces = [MediaType.TEXT_HTML])
-    fun index(): String? {
-        return javaClass.getResourceAsStream("/web/index.html")
-            ?.readBytes()
-            ?.toString(Charset.defaultCharset())
-    }
+    fun public(): String? = resource
+
+    @Get(uri = "/shop", produces = [MediaType.TEXT_HTML])
+    fun shop(): String? = resource
+
+    @Get(uri = "/login", produces = [MediaType.TEXT_HTML])
+    fun login(): String? = resource
+
+    @Get(uri = "/register/user", produces = [MediaType.TEXT_HTML])
+    fun register(): String? = resource
+
+    @Get(uri = "/profile", produces = [MediaType.TEXT_HTML])
+    fun profile(): String? = resource
+
+    @Get(uri = "/CustomerHistory", produces = [MediaType.TEXT_HTML])
+    fun customerHistory(): String? = resource
+
+    @Get(uri = "/customerhistory", produces = [MediaType.TEXT_HTML])
+    fun CustomerHistory(): String? = resource
+
+    @Get(uri = "/customerrentorders", produces = [MediaType.TEXT_HTML])
+    fun customerrentorders(): String? = resource
+
+
+//    @Get(uri = "/", produces = [MediaType.TEXT_HTML])
+//    fun shop(): String? {
+//        return javaClass.getResourceAsStream("/web/index.html")
+//            ?.readBytes()
+//            ?.toString(Charset.defaultCharset())
+//    }
+
 }
+
