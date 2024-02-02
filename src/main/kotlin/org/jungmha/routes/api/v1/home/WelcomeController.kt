24b759a326780fa2ee0777893bf67ed5c30de839
rushmi0 <gcalku@gmail.com>
2024-02-02 16:20:56 +0700

    รองรับ Static Resources

--- src/main/kotlin/org/jungmha/routes/api/v1/home/WelcomeController.kt
+++ src/main/kotlin/org/jungmha/routes/api/v1/home/WelcomeController.kt
@@ -2,17 +2,22 @@ package org.jungmha.routes.api.v1.home
 
 
 import io.micronaut.context.annotation.Bean
-import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.MediaType
+import io.micronaut.runtime.http.scope.RequestScope
+import io.micronaut.scheduling.TaskExecutors
+import io.micronaut.scheduling.annotation.ExecuteOn
 import io.swagger.v3.oas.annotations.tags.Tag
 import java.nio.charset.Charset
 
 @Tag(
-    name = "CamRent",
-    description = "เว็บตัวอย่าง CamRent"
+    name = "Jungmha Wab",
+    description = "หน้าเว็บทั้งหมดของ Jungmha"
 )
 @Bean
+@RequestScope
+@ExecuteOn(TaskExecutors.IO)
 @Controller("/")
 class WelcomeController {
 
@@ -20,37 +25,7 @@ class WelcomeController {
         ?.readBytes()
         ?.toString(Charset.defaultCharset())
 
-    @Get(uri = "/", produces = [MediaType.TEXT_HTML])
-    fun public(): String? = resource
 
-    @Get(uri = "/shop", produces = [MediaType.TEXT_HTML])
-    fun shop(): String? = resource
-
-    @Get(uri = "/login", produces = [MediaType.TEXT_HTML])
-    fun login(): String? = resource
-
-    @Get(uri = "/register/user", produces = [MediaType.TEXT_HTML])
-    fun register(): String? = resource
-
-    @Get(uri = "/profile", produces = [MediaType.TEXT_HTML])
-    fun profile(): String? = resource
-
-    @Get(uri = "/CustomerHistory", produces = [MediaType.TEXT_HTML])
-    fun customerHistory(): String? = resource
-
-    @Get(uri = "/customerhistory", produces = [MediaType.TEXT_HTML])
-    fun CustomerHistory(): String? = resource
-
-    @Get(uri = "/customerrentorders", produces = [MediaType.TEXT_HTML])
-    fun customerrentorders(): String? = resource
-
-
-//    @Get(uri = "/", produces = [MediaType.TEXT_HTML])
-//    fun shop(): String? {
-//        return javaClass.getResourceAsStream("/web/index.html")
-//            ?.readBytes()
-//            ?.toString(Charset.defaultCharset())
-//    }
 
 }
 
