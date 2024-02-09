16d739fc196cc69059b753a917e9d808d66e30c8
rushmi0 <gcalku@gmail.com>
2024-02-09 09:11:21 +0700

    สร้างค่าคงที่

---
+++ constants/BaseEndpoint.js
@@ -0,0 +1,7 @@
+
+
+const BASE_URL_USER = "http://localhost:8080/api/v1/user"
+//const BASE_URL_USER = "http://10.0.2.2:8080/api/v1/user"
+
+const BASE_URL_DOG = "http://localhost:8080/api/v1/dogs"
+//const BASE_URL_DOG = "http://10.0.2.2:8080/api/v1/dogs"
