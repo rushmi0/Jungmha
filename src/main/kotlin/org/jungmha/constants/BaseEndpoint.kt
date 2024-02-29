6706212f28e3cc0fadd2f4919829a180110b1272
milko <ph.watcharapol_st@tni.ac.th>
2024-02-29 21:03:10 +0700

    กำหนด Base Endpoint เป็นชื่อ domain

--- src/main/kotlin/org/jungmha/constants/BaseEndpoint.kt
+++ src/main/kotlin/org/jungmha/constants/BaseEndpoint.kt
@@ -5,11 +5,11 @@ import io.micronaut.core.annotation.Introspected
 @Introspected
 object BaseEndpoint {
 
-    val BASE_URL_USER = "https://jungmha.notoshi.win"
+    val BASE_URL_USER = "https://jungmha.notoshi.win/"
     //val BASE_URL_USER = "http://localhost:8080/api/v1/user"
     //val BASE_URL_USER = "http://10.0.2.2:8080/api/v1/user"
 
-    val BASE_URL_DOG = "https://jungmha.notoshi.win"
+    val BASE_URL_DOG = "https://jungmha.notoshi.win/"
     //val BASE_URL_DOG = "http://localhost:8080/api/v1/dogs"
     //val BASE_URL_DOG = "http://10.0.2.2:8080/api/v1/dogs"
 
