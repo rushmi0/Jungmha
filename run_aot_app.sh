55540ccb12ff5ccac75a57c7d834819d58c07078
milko <gcalku@gmail.com>
2024-04-08 20:51:18 +0700

    แก้ไขกำหนดค่าเล็กน้อย

--- run_aot_app.sh
+++ run_aot_app.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# ตรวจสอบว่ามี dir ชื่อ build/native/nativeOptimizedCompile หรือไม่
-if [ ! -d "build/native/nativeOptimizedCompile" ]; then
-    # ถ้าไม่มีให้รันคำสั่ง ./gradlew nativeOptimizedCompile
+if [ ! -f "build/native/nativeOptimizedCompile/jungmha-0.0.1-alpha" ]; then
     ./gradlew nativeOptimizedCompile
 
-    # เมื่อเสร็จสิ้นให้ cd เข้าไปใน build/native/nativeOptimizedCompile
     cd build/native/nativeOptimizedCompile || exit
 else
-    # ถ้ามีอยู่แล้วให้เข้าไปใน build/native/nativeOptimizedCompile
     cd build/native/nativeOptimizedCompile || exit
 fi
 
-# หลังจากเข้าไปใน build/native/nativeOptimizedCompile ให้รัน ./jungmha-0.0.1-alpha
 ./jungmha-0.0.1-alpha
