a9e471498ef3788e0e2eecb5d84ae94fb6f7cd55
milko <gcalku@gmail.com>
2024-04-08 23:53:07 +0700

    แก้ไขกำหนดค่า

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,13 +65,13 @@ dependencies {
     // https://mvnrepository.com/artifact/com.google.guava/guava
     implementation("com.google.guava:guava:32.1.2-jre")
 
-
 }
 
 
 graalvmNative {
     binaries {
         all {
+            // * https://www.graalvm.org/latest/reference-manual/native-image/overview/BuildOutput/?fbclid=IwAR007Rh7fYg-CJZywqhFM8PF5XDWPvgOfaV9txFDqpy6PWjtZp2bXpgncL0_aem_Af0UTqW_wKY5RFkebOwqrANSJn-d6fpSoJLMyra23KLgMNQuur3l75gjN29_Ymw1JYkeX7upxGBzGPFkJ4iRuojh
             buildArgs.add("-H:+AddAllCharsets")
             buildArgs.add("-R:MaxHeapSize=2G")
             imageName.set("${project.name}-0.0.1-alpha")
